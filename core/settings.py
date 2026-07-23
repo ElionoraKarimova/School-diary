ca6934b36cb5adbc31d2fca41f6096b4b1e26eea
ElionoraKarimova <elionorakarimova94@gmail.com>
2026-07-23 13:21:54 +0200

    Migrate database setup from SQLite to PostgreSQL with Docker Compose

--- core/settings.py
+++ core/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'school_db'),
+        'USER': os.environ.get('POSTGRES_USER', 'school_user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'school_password'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
